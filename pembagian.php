c309b38fe4d40625cfe3c2793985e7a4e4a5dc74
KhanifulHuda21 <70567730+KhanifulHuda21@users.noreply.github.com>
2020-09-17 02:41:43 -0700

    update pembagian

--- pembagian.php
+++ pembagian.php
@@ -1,33 +1,4 @@
 <?php
-
-
-$keluar=true;
-
-while($keluar)
-{
-
-    echo "aplikasi perhitungan !!!\n";
-
-    $angka_pertama=readline("masukkan diameter lingkaran : ");
-    
-
-    if(!is_numeric($angka_pertama))
-    {
-        echo "maaf input hanya bisa angka !!!\n";
-        continue;
-    }
-
-    echo "keliling lingkaran yang berdiameter $angka_pertama adalah : ".($angka_pertama*(22/7))."\n";
-
-    // $ulangi=readline("masuk pilihan lain \n 1.pertambahan \n 2.pengurangan \n 3.perkalian \n 4.pembagian \n5.keliling lingkaran \t :");
-
-    $ulangi=readline("ulangi lagi (yes/no) \t :");
-
-    if($ulangi=="no"|| $ulangi!=="yes")
-    {
-        $keluar=false;
-    }
-};
 $keluar=true;
 
 
